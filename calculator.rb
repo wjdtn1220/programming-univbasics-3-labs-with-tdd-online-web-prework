f51330d9a2906199823aa40666a3fc0b49bae99a
Jeong Park <wjdtn1220@gmail.com>
2019-07-24 01:44:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 0
-expect(second_number).not_to equal(0)
+first_number = 1
+second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
