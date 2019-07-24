f076ac880addf53cb63b6bea201ec8b7f7ba8939
Jeong Park <wjdtn1220@gmail.com>
2019-07-24 01:36:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 0
 expect(second_number).not_to equal(0)
-sum = first_number && second_number
+sum = first_number + second_number
+difference = second_number - first_number
+product = first_number * second_number
+quotient = second_number / first_number
